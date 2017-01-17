da49de005cc675bd6d886cdf73f743e7f61207ad
fengmk2 <fengmk2@gmail.com>
2017-01-17 15:00:27 +0800

    feat: only use server listen to detect port
    use npm scripts instead of Makefile

--- test/detect-port.test.js
+++ test/detect-port.test.js
@@ -1,78 +1,86 @@
 'use strict';
 
+const assert = require('assert');
+const net = require('net');
 const detectPort = require('..');
 
 describe('detect port test', () => {
+  let server;
+  before(done => {
+    server = new net.Server();
+    server.listen(7001, done);
+  });
+  after(() => {
+    server.close();
+  });
+
+  it('get random port', done => {
+    detectPort((_, port) => {
+      assert(port >= 1024 && port < 65535);
+      done();
+    });
+  });
 
   it('callback with occupied port', done => {
     const _port = 80;
-    detectPort(_port, (err, port) => {
-      if (err) {
-        console.log(err);
-      }
-      port.should.within(_port, 65535);
+    detectPort(_port, (_, port) => {
+      assert(port >= _port && port < 65535);
+      done();
+    });
+  });
+
+  it('work with listening port', done => {
+    const port = 7001;
+    detectPort(port, (_, realPort) => {
+      assert(realPort !== 7001);
+      assert(realPort > 0);
       done();
     });
   });
 
   it('callback with string arg', done => {
     const _port = '8080';
-    detectPort(_port, (err, port) => {
-      if (err) {
-        console.log(err);
-      }
-      port.should.within(parseInt(_port, 10), 65535);
+    detectPort(_port, (_, port) => {
+      assert(port >= 8080 && port < 65535);
       done();
     });
   });
 
   it('callback with wrong arguments', done => {
-    detectPort('oooo', err => {
-      err.should.containEql('wrong type of arguments');
+    detectPort('oooo', (_, port) => {
+      assert(port > 0);
       done();
     });
   });
 
-  it('generator usage', function *() {
+  it('generator usage', function* () {
     const _port = 8080;
-    try {
-      const port = yield detectPort(_port);
-      port.should.within(_port, 65535);
-    } catch (err) {
-      console.log(err);
-    }
+    const port = yield detectPort(_port);
+    assert(port >= _port && port < 65535);
   });
 
   it('promise usage', done => {
     const _port = 8080;
     detectPort(_port)
       .then(port => {
-        port.should.within(_port, 65535);
+        assert(port >= _port && port < 65535);
         done();
       })
-      .catch(err => {
-        console.log(err);
-        done();
-      });
+      .catch(done);
   });
 
   it('promise with wrong arguments', done => {
     detectPort()
-      .then(() => {
+      .then(port => {
+        assert(port > 0);
         done();
       })
-      .catch(err => {
-        err.should.containEql('wrong number of arguments');
-        done();
-      });
+      .catch(done);
   });
 
-  it('generator with wrong arguments', function *() {
-    try {
-      yield detectPort('oooo');
-    } catch (err) {
-      err.should.containEql('wrong type of arguments');
-    }
+  it('generator with wrong arguments and return random port', function* () {
+    const port = yield detectPort('oooo');
+    assert(port > 0);
+    assert(typeof port === 'number');
   });
-
 });
